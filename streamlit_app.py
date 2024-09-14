3a2f12874241c16dcfd460a7cd666a2824160592
Srujana <siyyadrisivasrujana@gmail.com>
2024-09-14 16:27:37 +0530

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,7 +122,7 @@ def video():
                         threshold=cv2.threshold(gray_image1,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
                         #rem_noise=cv2.medianBlur(threshold,5)
                         #PyTesseract to convert text in the image to string
-                        ?plate = pytesseract.image_to_string(threshold, config='-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8')
+                        # plate = pytesseract.image_to_string(threshold, config='-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8')
                         reader = easyocr.Reader(['en'],model_storage_directory='.')
                         plate = reader.readtext(threshold,paragraph="False")[0][1]
                         if(plate==''):
