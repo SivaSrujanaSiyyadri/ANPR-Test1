ff81e07e3b0f328f82fb41bd65c9547d0e001b1c
Srujana <siyyadrisivasrujana@gmail.com>
2024-09-15 09:14:53 +0530

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,9 +122,9 @@ def video():
                         threshold=cv2.threshold(gray_image1,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
                         #rem_noise=cv2.medianBlur(threshold,5)
                         #PyTesseract to convert text in the image to string
-                        # plate = pytesseract.image_to_string(threshold, config='-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8')
-                        reader = easyocr.Reader(['en'],model_storage_directory='.')
-                        plate = reader.readtext(threshold,paragraph="False")[0][1]
+                        plate = pytesseract.image_to_string(threshold, config='-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8')
+                        # reader = easyocr.Reader(['en'],model_storage_directory='.')
+                        # plate = reader.readtext(threshold,paragraph="False")[0][1]
                         if(plate==''):
                             plate="Error"
                         if(len(plate)>10): #Car number plate can not have more than 10 characters 
